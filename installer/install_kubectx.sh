30cea27d006142860e422f03a3f55b58c65ea444
Dwi Fahni Denni <dfdenni0912@gmail.com>
2023-04-05 13:01:00 +0700

    Update script user data installation

--- installer/install_kubectx.sh
+++ installer/install_kubectx.sh
@@ -7,8 +7,8 @@ export KUBECTX_PATH="$GOPATH/src/github.com/kubectx"
 
 git clone https://github.com/ahmetb/kubectx $KUBECTX_PATH
 
-ln -snf $KUBECTX_PATH/kubectx /usr/local/bin/kubectx
-ln -snf $KUBECTX_PATH/kubectx /usr/local/bin/kx
+sudo ln -snf $KUBECTX_PATH/kubectx /usr/local/bin/kubectx
+sudo ln -snf $KUBECTX_PATH/kubectx /usr/local/bin/kx
 
-ln -snf $KUBECTX_PATH/kubens /usr/local/bin/kubens
-ln -snf $KUBECTX_PATH/kubens /usr/local/bin/ks
+sudo ln -snf $KUBECTX_PATH/kubens /usr/local/bin/kubens
+sudo ln -snf $KUBECTX_PATH/kubens /usr/local/bin/ks
